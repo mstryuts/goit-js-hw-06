00aeba0789076f17b3a6e5d1757ebe76cd5be5cf
mstryuts <maxim.stryuts@gmail.com>
2022-07-21 19:15:57 +0300

    first task done

---
+++ js/task-01.js
@@ -0,0 +1,9 @@
+
+
+const arrCategories = document.querySelectorAll('.item')
+console.log(`Number of categories: ${arrCategories.length}`)
+
+arrCategories.forEach((el)=> {
+    console.log(`Category: ${el.firstElementChild.textContent}`);
+    console.log(`Elements: ${el.lastElementChild.children.length}`)
+})
