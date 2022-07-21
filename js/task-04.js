9ccaffc5df0f22b83556597377884072e58701f5
mstryuts <maxim.stryuts@gmail.com>
2022-07-22 00:13:12 +0300

    task 4 done

---
+++ js/task-04.js
@@ -0,0 +1,18 @@
+let counterValue = 0;
+const incr = document.querySelector('button[data-action="increment"]');
+const decr = document.querySelector('button[data-action="decrement"]');
+let value = document.querySelector('#value')
+
+const onClickDecr = () =>{
+    counterValue -= 1
+    value.textContent = counterValue
+}
+
+const onClickIncr = () =>{
+    counterValue += 1
+    value.textContent = counterValue
+}
+
+incr.addEventListener('click', onClickIncr)
+decr.addEventListener('click', onClickDecr)
+
