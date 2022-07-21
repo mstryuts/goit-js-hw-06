22994e68b2facd13ac145d27624458aff805e18a
mstryuts <maxim.stryuts@gmail.com>
2022-07-21 19:44:16 +0300

    task 2 done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector('#ingredients')
+
+const ingrEl = ingredients.map((el) => {
+  const ingredientEl = document.createElement('li');
+  ingredientEl.textContent = el;
+  ingredientEl.classList.add = 'item';
+  
+  return ingredientEl
+})
+
+ingredientsList.append(...ingrEl)
